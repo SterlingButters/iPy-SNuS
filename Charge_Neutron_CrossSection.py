1bf5edfc540c1cfe34dc56757cb00a3b714d40d3
Jaafar EL Bakkali <bahmedj@gmail.com>
2021-01-20 18:34:25 +0100

    Add files via upload

---
+++ Charge_Neutron_CrossSection.py
@@ -0,0 +1,10 @@
+import numpy as np
+import numpy.random as rndg
+import sys
+Energy_H, Sigma_Total_H, Sigma_Elastic_H, Sigma_Absorption_H = np.loadtxt('./data/1-H.csv', delimiter=';', unpack=True)
+Energy_O, Sigma_Total_O, Sigma_Elastic_O, Sigma_Absorption_O = np.loadtxt('./data/16-O.csv', delimiter=';', unpack=True)
+nb_H = len(Energy_H) 
+nb_O = len(Energy_O) 
+print("=== Number of cross-section data for isotope 1-H: ",nb_H)
+print("=== Number of cross-section data for isotope 16-O: ",nb_O)
+
